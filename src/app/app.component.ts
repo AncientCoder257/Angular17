0de069f1bc74a08ceaa378bc4490ff2d20b43fec
Abdul Charfaray <abdulc@runninghill.co.za>
2024-04-04 14:56:36 +0200

    basic buttons.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatAnchor, MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButton, MatAnchor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
